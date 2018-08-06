bb15638899e12abf30bba20d0fe01abe9198a028
Gustavo Antonio Lastra Colorado <gustavo.lastrac@gmail.com>
2018-08-06 20:35:41 +0200

    add githubpages

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/weihnachtsDOM-vue',
+    assetsPublicPath: './weihnachtsDOM-vue',
 
     /**
      * Source Maps
